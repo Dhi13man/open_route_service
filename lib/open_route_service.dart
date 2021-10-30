c3194c842c126ef6fd6961a03e93cd489013f4ab
Dhiman Seal <furyx.ds@gmail.com>
2021-10-31 00:26:02 +0530

    Dart format for v1.0.1 :)

--- lib/open_route_service.dart
+++ lib/open_route_service.dart
@@ -1,5 +1,5 @@
 /// An encapsulation made around OpenRouteService API for Dart/Flutter projects.
-/// 
+///
 /// Made for easy generation of Directions, Isochrones, Time-Distance Matrix,
 /// Pelias Geocoding, etc. using their amazing API.
 library open_route_service;
